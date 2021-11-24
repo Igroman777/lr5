ef06c2f7cc76be05e97d90c1fd7dbb440db7be90
Igor 777 <was8046@gmail.com>
2021-11-24 15:53:35 +0200

    Добавьте файлы проекта.

---
+++ lr5/Form2.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace lr5
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+    }
+}
